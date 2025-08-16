519837deb0317be6bce7ab1ade2496a2a98aea18
DevRafaelLuz <rafael.luz2005@gmail.com>
2025-08-16 15:29:49 -0300

    Menu ajustado

--- src/views/MenuPrincipal.py
+++ src/views/MenuPrincipal.py
@@ -1,24 +1,48 @@
 from interfaces.Menu import Menu
+from models.Classico import Classico
+from models.Aventureiro import Aventureiro
 from models.Heroico import Heroico
 
 class MenuPrincipal(Menu):
     def exibir_menu(self):
-        print("Bem-vindo ao Menu Principal!")
-
+        classico = Classico()
+        aventureiro = Aventureiro()
         heroico = Heroico()
+
+        print("Bem-vindo ao Menu Principal!")
 
         while True:
             try:
-                print("1. Criar Personagem")
+                print("1. Novo Personagem")
                 print("2. Sair")
                 
                 opcao = int(input("Escolha uma opção: "))
                 
                 match opcao:
                     case 1:
-                        heroico.rolar_dados()
-                        heroico.definir_atributos()
-                        print(heroico.exibir_atributos())
+                        try:
+                            print("1. Estilo Clássico")
+                            print("2. Estilo Aventureiro")
+                            print("3. Estilo Heroico")
+
+                            estilo = int(input("Escolha o estilo do personagem: "))
+
+                            match estilo:
+                                case 1:
+                                    classico.definir_atributos()
+                                    print(classico.exibir_atributos())
+                                case 2:
+                                    aventureiro.rolar_dados()
+                                    aventureiro.definir_atributos()
+                                    print(aventureiro.exibir_atributos())
+                                case 3:
+                                    heroico.rolar_dados()
+                                    heroico.definir_atributos()
+                                    print(heroico.exibir_atributos())
+                                case _:
+                                    print("Estilo inválido. Tente novamente.")
+                        except ValueError:
+                            print("Entrada inválida. Por favor, insira um número.")
                     case 2:
                         break
                     case _:
