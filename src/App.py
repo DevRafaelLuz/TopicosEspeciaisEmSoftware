50fc88134ede8cd041fee2af209f72c4db20196d
DevRafaelLuz <rafael.luz2005@gmail.com>
2025-09-07 14:37:05 -0300

    Desing inicial da página de criação de personagem desenvolvida

--- src/App.py
+++ src/App.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/criacao-personagem')
+def criacao_personagem():
+    return render_template('criacao_personagem.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
